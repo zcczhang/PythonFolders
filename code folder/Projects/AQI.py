637e11e961a279f362e52e846a09debf649335d8
CZ <2265981775@qq.com>
2019-07-14 19:47:12 +0800

    update 2019.7.14

--- code folder/Projects/AQI.py
+++ code folder/Projects/AQI.py
@@ -1,27 +1,53 @@
 # Author: CZ
-# Time: 2019-07-13 00:07
+# Time: 2019-07-13 23:23
 
 import requests
 from bs4 import BeautifulSoup
-from easygui import *
+import csv
 
 
-def get_one_page(city):
-    url = "http://www.air-level.com/air/" + city + '/'
-    kv = {'user-agent': 'Mozilla/5.0'}
-    r = requests.get(url, headers=kv)
-    r.raise_for_status()
-    r.encoding = r.apparent_encoding
-    demo = r.text
-    soup = BeautifulSoup(demo, "html.parser")
-    s = soup.find("span")
-    return s.string
+def get_city_aqi(url):
+    aqi_list = []
+    r = requests.get(url, timeout=30)
+    soup = BeautifulSoup(r.text, 'lxml')
+    div_val = soup.find_all('td', {'class': 'aqiwgt-table-aqicell'})[0]
+    aqivalue = div_val.find('div', {'class': 'aqivalue'}).text
+    aqi_list.append(aqivalue)
+
+    timediv = soup.find_all('div', {'style': 'font-size:16px;font-weight:light;;'})[0]
+    tdiv = timediv.find('script').text
+    timestr = tdiv.split("'")[1].strip()
+    aqi_list.append(timestr)
+    return aqi_list
+
+
+def get_cities_url():
+    allaqilist = []
+    url = 'http://aqicn.org/city/all'
+    r = requests.get(url, timeout=30)
+    soup = BeautifulSoup(r.text, 'lxml')
+    div_all = soup.find_all('div', {'class': 'main-cities'})[0]
+    citylinklist = div_all.find_all('a')
+    for i in range(392):
+        allaqilist.append((citylinklist[i].get('href'), citylinklist[i].text))   # (url, cityname)
+    return allaqilist
 
 
 def main():
-    ipt = multenterbox(msg='Fill the Chinese City Name in Pinyin', title='Get the AQI',
-                       fields=['City Name: '])
-    msgbox(msg=get_one_page(ipt[0]), title='AQI in %s'%ipt[0])
+    city_list = get_cities_url()
+    header = ['City', 'AQI', 'Update Time(local time)']
+
+    with open('Real Time AQI of Major Cities in the World.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        for i, city in enumerate(city_list):
+            if (i + 1) % 10 == 0:
+                print('Already got {} data. ({} in total)'.format(i + 1, len(city_list)))
+            url = city[0]
+            name = city[1]
+            city_aqi = get_city_aqi(url)
+            row = [name] + city_aqi
+            writer.writerow(row)
 
 
 if __name__ == '__main__':
